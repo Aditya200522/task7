5702a707d61bf47c17725a959435c12fd4291151
Aditya200522 <gottapudinesh7@gmail.com>
2025-03-12 11:11:42 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,24 @@
+class family:
+    def parent(self):
+        print("This is the Parent class")
+
+
+class Child1(family):
+    def see_info(self):
+        print("Feature from Child1")
+
+
+class Child2(Child1):
+    def see_info1(self):
+        print("Feature from Child2")
+
+
+obj1 = Child1()
+obj2 = Child2()
+
+
+obj1.parent()      
+obj1.see_info()  
+
+obj2.parent()      
+obj2.see_info1() 
